16885479ac8be067d0d4ed904ca54fd1f857327f
Seimi Terasaki <seimiterasaki@gmail.com>
2016-11-10 15:38:40 +0000

    initial commit

--- app/auth/config.js
+++ app/auth/config.js
@@ -2,7 +2,7 @@ var ids = {
   github: {
     clientID: process.env.GITHUB_ID,
     clientSecret: process.env.GITHUB_KEY,
-    callbackURL: "https://fcc-nightlifeapp-sterasaki.c9users.io/auth/github/callback"
+    callbackURL: "https://sterasaki-nightlifeapp.herokuapp.com/auth/github/callback"
   }
 };
 
